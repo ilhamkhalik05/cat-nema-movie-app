73e12e71f94a1b457506e2613906002c3c082a8a
Ilham Khalik <believerrr05@gmail.com>
2024-10-10 17:10:26 +0700

    refactor: Move brand logo assets directive

--- src/components/navbar/brand-logo.tsx
+++ src/components/navbar/brand-logo.tsx
@@ -1,11 +1,11 @@
-import logo from '@/assets/logo.png';
+import { brandLogo } from '@/lib/assets';
 import Image from 'next/image';
 
 export function BrandLogo() {
   return (
     <div className="flex items-center gap-5">
       <div className="rounded-3xl p-2 shadow-lg shadow-zinc-900">
-        <Image className="size-12 object-contain" src={logo.src} alt={'Logo'} width={1000} height={1000} />
+        <Image className="size-12 object-contain" src={brandLogo.src} alt={'Logo'} width={1000} height={1000} />
       </div>
       <h1 className="text-3xl text-foreground font-[900]">CatNema</h1>
     </div>
---
+++ src/lib/assets.ts
@@ -0,0 +1,3 @@
+import brandLogo from '@/assets/logo.png';
+
+export { brandLogo };
