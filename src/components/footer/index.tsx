f92fc4cab8cab2dc9f056d772def4ef600d94cf0
Ilham Khalik <believerrr05@gmail.com>
2024-10-11 17:29:14 +0700

    ui: Add footer component (need to improve)

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="px-8 py-6 text-center w-full bg-zinc-900/80 font-[600]">
+      All rights reserved &copy; 2024 - Ilham Khalik
+    </footer>
+  );
+}
