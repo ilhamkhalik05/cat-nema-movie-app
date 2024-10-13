ee3556e8c231b6df723a3bdaf5a170721d89df5a
Ilham Khalik <believerrr05@gmail.com>
2024-10-13 17:55:29 +0700

    ui/adjust: Adjust navbar backdrop blur

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,7 +5,7 @@ import { BrandLogo } from '../brand-logo';
 
 export default function Navbar() {
   return (
-    <header className="z-10 fixed top-0 flex justify-between items-center py-5 px-8 backdrop-blur-xl w-full">
+    <header className="z-10 fixed top-0 flex justify-between items-center py-5 px-8 backdrop-blur-lg w-full">
       {/* Left Side */}
       <div className="flex items-center gap-14">
         <BrandLogo />
