7c6bba2d3b2c9cc241af3832c0183dca5258fb95
Ilham Khalik <believerrr05@gmail.com>
2024-10-16 18:59:40 +0700

    feat: Add fetch movie watchlist service

---
+++ src/services/movie/fetchMovieWatchlist.ts
@@ -0,0 +1,25 @@
+import { handleFetchApiError } from '@/lib/api';
+import { API_BASE_URL, API_BASE_URL_VERSION, API_KEY } from '@/lib/env';
+import { Movie } from '@/lib/type';
+import axios from 'axios';
+
+export async function fetchMovieWatchlist(sessionId: string): Promise<Movie[] | null> {
+  try {
+    const url = `${API_BASE_URL}/${API_BASE_URL_VERSION}/account/null/watchlist/movies?session_id=${sessionId}&sort_by=created_at.asc&api_key=${API_KEY}`;
+
+    const res = await axios.get(url, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (res.status !== 200) {
+      handleFetchApiError('fetch user movie watchlist', res.statusText);
+    }
+
+    return res.data.results;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
--- src/services/movie/index.ts
+++ src/services/movie/index.ts
@@ -2,3 +2,4 @@ export * from './fetchNowPlayingMovies';
 export * from './fetchPopularMovies';
 export * from './fetchTopRatedMovies';
 export * from './fetchMovieDetailsById';
+export * from './fetchMovieWatchlist';
