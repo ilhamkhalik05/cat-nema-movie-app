b3f6b5f843f367181123c6b5c2dd1b4d29dcda66
Ilham Khalik <believerrr05@gmail.com>
2024-10-20 17:15:00 +0700

    fix: Fix import path for toast provider css

--- src/app/toast-provider.tsx
+++ src/app/toast-provider.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { poppins } from '@/lib/font';
 import { ToastContainer } from 'react-toastify';
 
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/ReactToastify.css';
 import { cn } from '@/lib/utils';
 
 export default function ToastProvider() {
